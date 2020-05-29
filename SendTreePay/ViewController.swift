6963d2d2f493d17d8f5cf7b898c7965360804500
Miguel <gannasong@gmail.com>
2020-05-29 13:58:29 +0800

    init cocoapods

---
+++ SendTreePay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendTreePay
+//
+//  Created by SUNG HAO LIN on 2020/5/29.
+//  Copyright © 2020 SUNG HAO LIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
